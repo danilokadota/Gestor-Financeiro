aeb50a86ce304aeffa9dcc56d7b8458f9be476cc
danilokadota <danilo.kadota@dclick.com.br>
2015-01-21 14:29:00 -0200

    teste

---
+++ app/src/main/java/br/com/brunokadota/gestorfinaceiro/MenuActivity.java
@@ -0,0 +1,17 @@
+package br.com.brunokadota.gestorfinaceiro;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        
+
+    }
+
+}
